513bc0f54df8177b0917726099e337c68d954d24
Luis <luishp.23@gmail.com>
2014-05-20 22:16:22 -0600

    Metodo eliminar de tabla RegistrarBitacora

--- TareaGrupo18/src/sv/ues/fia/registrobitacora/RegistroBitacoraEliminar.java
+++ TareaGrupo18/src/sv/ues/fia/registrobitacora/RegistroBitacoraEliminar.java
@@ -1,25 +1,34 @@
 package sv.ues.fia.registrobitacora;
 
+import sv.ues.fia.ControladorBDG18;
 import sv.ues.fia.R;
-import sv.ues.fia.R.layout;
-import sv.ues.fia.R.menu;
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegistroBitacoraEliminar extends Activity {
-
+	EditText editIdBitacora,editcarnet;
+	ControladorBDG18 helper;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_registro_bitacora_eliminar);
+		helper=new ControladorBDG18(this);
+		editIdBitacora=(EditText)findViewById(R.id.editIdBitacora);
+		editcarnet=(EditText)findViewById(R.id.editcarnet);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.registro_bitacora_eliminar, menu);
-		return true;
+	public void eliminarRegistroBitacora(View v){
+		String regEliminadas;
+		RegistroBitacora rbit =new RegistroBitacora();
+		rbit.setIdbitacora(Integer.parseInt(editIdBitacora.getText().toString()));
+		rbit.setCarnet(editcarnet.getText().toString());
+		helper.abrir();
+		regEliminadas=helper.eliminar(rbit);
+		helper.cerrar();
+		Toast.makeText(this, regEliminadas, Toast.LENGTH_SHORT).show();
 	}
 
 }
